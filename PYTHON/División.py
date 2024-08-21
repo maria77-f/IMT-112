13c3c493649dd5f9e3f3667b87c1d508b1ff7fe9
mari-fc7 <maria.flores.c@ucb.edu.bo>
2024-08-21 18:51:36 -0400

    21Agust

---
+++ PYTHON/División.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+def division_entera(a,b):
+    return a//b
+def division_decimal(a,b):
+    return a/b
+
+print (division_entera(a,b))
+print (division_decimal(a,b))
---
+++ PYTHON/f.print.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    if 1 <= n <= 150:
+        for i in range (n):
+             print(i+1, end="")
